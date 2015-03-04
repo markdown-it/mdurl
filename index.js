8a330770b5f5f5e7e9cb387a83c1ceb95f29150e
Alex Kocharin <alex@kocharin.ru>
2015-03-04 13:44:55 +0300

    Add decode function

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 
 module.exports.encode = require('./encode');
+module.exports.decode = require('./decode');
 module.exports.format = require('./format');
 module.exports.parse  = require('./parse');
