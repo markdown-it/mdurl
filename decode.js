decaec487133ae3751f18701fead4c987e1c32d7
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-09-15 01:04:48 +0300

    Fixed closure compiler compatibility

--- decode.js
+++ decode.js
@@ -38,7 +38,7 @@ function decode(string, exclude) {
   cache = getDecodeCache(exclude);
 
   return string.replace(/(%[a-f0-9]{2})+/gi, function(seq) {
-    var i, l, b1, b2, b3, b4, char,
+    var i, l, b1, b2, b3, b4, chr,
         result = '';
 
     for (i = 0, l = seq.length; i < l; i += 3) {
@@ -54,12 +54,12 @@ function decode(string, exclude) {
         b2 = parseInt(seq.slice(i + 4, i + 6), 16);
 
         if ((b2 & 0xC0) === 0x80) {
-          char = ((b1 << 6) & 0x7C0) | (b2 & 0x3F);
+          chr = ((b1 << 6) & 0x7C0) | (b2 & 0x3F);
 
-          if (char < 0x80) {
+          if (chr < 0x80) {
             result += '\ufffd\ufffd';
           } else {
-            result += String.fromCharCode(char);
+            result += String.fromCharCode(chr);
           }
 
           i += 3;
@@ -73,12 +73,12 @@ function decode(string, exclude) {
         b3 = parseInt(seq.slice(i + 7, i + 9), 16);
 
         if ((b2 & 0xC0) === 0x80 && (b3 & 0xC0) === 0x80) {
-          char = ((b1 << 12) & 0xF000) | ((b2 << 6) & 0xFC0) | (b3 & 0x3F);
+          chr = ((b1 << 12) & 0xF000) | ((b2 << 6) & 0xFC0) | (b3 & 0x3F);
 
-          if (char < 0x800 || (char >= 0xD800 && char <= 0xDFFF)) {
+          if (chr < 0x800 || (chr >= 0xD800 && chr <= 0xDFFF)) {
             result += '\ufffd\ufffd\ufffd';
           } else {
-            result += String.fromCharCode(char);
+            result += String.fromCharCode(chr);
           }
 
           i += 6;
@@ -93,13 +93,13 @@ function decode(string, exclude) {
         b4 = parseInt(seq.slice(i + 10, i + 12), 16);
 
         if ((b2 & 0xC0) === 0x80 && (b3 & 0xC0) === 0x80 && (b4 & 0xC0) === 0x80) {
-          char = ((b1 << 18) & 0x1C0000) | ((b2 << 12) & 0x3F000) | ((b3 << 6) & 0xFC0) | (b4 & 0x3F);
+          chr = ((b1 << 18) & 0x1C0000) | ((b2 << 12) & 0x3F000) | ((b3 << 6) & 0xFC0) | (b4 & 0x3F);
 
-          if (char < 0x10000 || char > 0x10FFFF) {
+          if (chr < 0x10000 || chr > 0x10FFFF) {
             result += '\ufffd\ufffd\ufffd\ufffd';
           } else {
-            char -= 0x10000;
-            result += String.fromCharCode(0xD800 + (char >> 10), 0xDC00 + (char & 0x3FF));
+            chr -= 0x10000;
+            result += String.fromCharCode(0xD800 + (chr >> 10), 0xDC00 + (chr & 0x3FF));
           }
 
           i += 9;
